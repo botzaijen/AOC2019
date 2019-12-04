16fae786d9e8b4174f3fc7712c9e8222817ef7b3
botzaijen <botzaijen@hotmail.com>
2019-12-04 09:33:11 +0100

    Part2 of day4 in Python

--- day4.py
+++ day4.py
@@ -15,12 +15,31 @@ def isValid(arr):
             dup = True
     return dup
 
-def part1():
+def isValid2(arr):
+    predup = False
+    dup = False
+    dupdig = -1
+    for i in range(1,len(arr)):
+        if (arr[i-1] > arr[i]):
+            return False
+        if (arr[i-1] == arr[i]):
+            if (arr[i] == dupdig):
+                dup = False
+            else:
+                dup = True
+                dupdig = arr[i]
+        else:
+            dupdig = -1
+            if dup:
+                predup = True
+    return dup or predup
+
+def part(validation_function):
     start, end = puzzle_input_arr
     okPassw = 0
     for passw in range(start, end+1):
         arr = splitToArray(passw)
-        if isValid(arr):
+        if validation_function(arr):
             okPassw = okPassw + 1
     print(f"Number of valid passwords in the range [{start}, {end}] = {okPassw}")
 
@@ -29,5 +48,8 @@ if __name__ == '__main__':
     #print(isValid(splitToArray(111111)))
     #print(isValid(splitToArray(223450)))
     #print(isValid(splitToArray(123789)))
-    part1()
+    #print(isValid2(splitToArray(112233)))
+    #print(isValid2(splitToArray(123444)))
+    #print(isValid2(splitToArray(111122)))
+    part(isValid2)
 
